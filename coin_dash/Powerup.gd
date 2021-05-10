27bafe6288a2b1172b9b1df4788068e2d58f75d7
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2021-05-10 23:44:35 +0700

    add coin dash game in Godot
    Gdscript
    with sound and animation

---
+++ coin_dash/Powerup.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+var screensize = Vector2()
+
+func pickup():
+	monitoring = false
+	$Tween.start()
+	
+func _ready():
+	$Tween.interpolate_property($AnimatedSprite, 'scale',
+								$AnimatedSprite.scale,
+								$AnimatedSprite.scale * 3, 0.3,
+								Tween.TRANS_QUAD,Tween.EASE_IN_OUT
+								)
+	
+	$Tween.interpolate_property($AnimatedSprite, 'modulate',
+								Color(1,1,1,1),
+								Color(1,1,1,0), 0.3,
+								Tween.TRANS_QUAD,
+								Tween.EASE_IN_OUT
+								)
+								
+
+func _on_Tween_tween_completed(object, key):
+	queue_free()
+
+func _on_Powerup_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_Lifetime_timeout():
+	queue_free()
